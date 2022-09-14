717e7c55a5e24d3e037cb3300ce4471b28ef25b7
Myoungmin <miung222@gmail.com>
2022-09-14 20:47:57 +0900

    Windows Messages : 윈도우 생싱 시 메시지 처리 실습
    WM_CREATE − When an object, called a window, is created, the frame that creates the objects sends a message identified as ON_WM_CREATE.
    
    Step 1 − To create ON_WM_CREATE, add afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct); before the DECLARE_MESSAGE_MAP()
    
    Step 2 − Add the ON_WM_CREATE() after the BEGIN_MESSAGE_MAP(CMainFrame, CFrameWnd) and before END_MESSAGE_MAP()
    
    Step 3 − Implementation of OnCreate()

--- MessagesAndEventsDemo.cpp
+++ MessagesAndEventsDemo.cpp
@@ -3,6 +3,8 @@ class CMainFrame : public CFrameWnd {
 public:
     CMainFrame();
 protected:
+    // Step 1 ? To create ON_WM_CREATE, add afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct); before the DECLARE_MESSAGE_MAP()
+    afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
     DECLARE_MESSAGE_MAP()
 };
 
@@ -12,12 +14,29 @@ CMainFrame::CMainFrame() {
         CRect(120, 100, 700, 480), NULL);
 }
 
+// Step 3 ? Implementation of OnCreate()
+int CMainFrame::OnCreate(LPCREATESTRUCT lpCreateStruct)
+{
+    // Call the base class to create the window
+    if (CFrameWnd::OnCreate(lpCreateStruct) == 0) {
+
+        // If the window was successfully created, let the user know
+        MessageBox(L"The window has been created!!!");
+        // Since the window was successfully created, return 0
+        return 0;
+    }
+    // Otherwise, return -1
+    return -1;
+}
+
 class CMessagesApp : public CWinApp {
 public:
     BOOL InitInstance();
 };
 
 BEGIN_MESSAGE_MAP(CMainFrame, CFrameWnd)
+    // Step 2 ? Add the ON_WM_CREATE() after the BEGIN_MESSAGE_MAP(CMainFrame, CFrameWnd) and before END_MESSAGE_MAP()
+    ON_WM_CREATE()
 END_MESSAGE_MAP()
 BOOL CMessagesApp::InitInstance() {
     m_pMainWnd = new CMainFrame;
